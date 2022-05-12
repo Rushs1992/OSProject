2bcd6daead610524165f5298af82e4d8dffb741c
rusha <rusha@172.16.3.214>
2022-05-12 12:42:42 -0700

    Burst time and arrival time are randomly added to processes

--- src/FinalProject/SimulateProcesses.java
+++ src/FinalProject/SimulateProcesses.java
@@ -1,35 +1,54 @@
 package FinalProject;
 
+import java.util.Random;
+
 public class SimulateProcesses {
 	
 	int processCount;
-	int counter = 0;
+	int pQcounter = 0;
+	int totalBurstTime = 0;
 	Process[] processQueue;
+	Process[] readyQueue;
 	
 	SimulateProcesses(int number){
 		
 		processCount = number;
 		processQueue = new Process[processCount];
+		readyQueue = new Process[processCount];
 		
 	} //end constructor
 	
 	public void addProcesses(Process P) {
 		
-		if(counter < processCount) {
-			processQueue[counter] = P;
-			counter++;
+		if(pQcounter < processCount) {
+			processQueue[pQcounter] = P;
+			totalBurstTime += processQueue[pQcounter].getBurstTime();
+			addArrivalTime(pQcounter);
+			pQcounter++;
 		} //end if
 		
 	} //end addProcesses
 	
 	public void printProcess() {
 		
-		System.out.println("\nProcesses Information\nID\tBurst Time\n");
+		System.out.println("\nProcesses Information\n\nID\tBurst Time\tArrival Time\n");
 		for(int i=0; i< processCount; i++) {
 			System.out.println(processQueue[i].PID + "\t" + 
-					processQueue[i].burstTime);
+					processQueue[i].burstTime + "\t\t" + processQueue[i].arrivalTime);
 		}//end for
-		
+		System.out.println("Total burst time:  " + totalBurstTime);
 	} //end printProcess
+	
+	public void addArrivalTime(int index) {
+		readyQueue[index] = processQueue[index];
+		if(index == 0) {
+			readyQueue[index].setArrivalTime(0);
+		}else {
+			Random rn = new Random();
+			int time = (1+ rn.nextInt(totalBurstTime/3));
+			//time = (int) (time%(0.8*totalBurstTime));
+			readyQueue[index].setArrivalTime(time);
+		} //end if-else
+	} // end addArrivalTime
 
 } //end class
