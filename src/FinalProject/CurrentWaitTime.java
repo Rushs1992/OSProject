3b2f1d59959f647630f4f4297f66b597a9692f6f
rusha <rusha@172.16.3.214>
2022-05-17 11:59:27 -0700

    New Class updated

--- src/FinalProject/CurrentWaitTime.java
+++ src/FinalProject/CurrentWaitTime.java
@@ -7,4 +7,19 @@ public class CurrentWaitTime {
 
 	Process[] readyQueue;
 	Queue<Process> test = new PriorityQueue<Process>();
+	CurrentWaitTime(){
+		
+	} //end constructor
+	
+	public void testfucn(SimulateProcesses S) {
+		
+		readyQueue = new Process[S.processCount];
+		for(int i = 0; i < S.processCount; i++) {
+			readyQueue[i] = S.processQueue[i];
+		}
+		for(int j=0; j<S.processCount;j++ ) {
+			test.add(readyQueue[j]);
+		}
+		System.out.println("1st element: " + test.remove().getPID());
 }
+}// end class
