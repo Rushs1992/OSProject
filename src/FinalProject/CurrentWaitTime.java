392e56ae253af0acd330b526e459345fd091cc73
rusha <rusha@172.16.3.214>
2022-05-17 11:24:04 -0700

    Removed QueueImp class

--- src/FinalProject/CurrentWaitTime.java
+++ src/FinalProject/CurrentWaitTime.java
@@ -1,7 +1,10 @@
 package FinalProject;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class CurrentWaitTime {
 
 	Process[] readyQueue;
-	
+	Queue<Process> test = new PriorityQueue<Process>();
 }
