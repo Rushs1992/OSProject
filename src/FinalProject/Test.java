ae09443b5187008b457dce73e934f3efe41124e2
Meet Shah <meetudiii@gmail.com>
2022-05-25 17:58:47 -0700

    updated SJF with the get methods and comments. updated Test with how to use the get methods.(commented)

--- src/FinalProject/Test.java
+++ src/FinalProject/Test.java
@@ -7,15 +7,15 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("\nRushang");
-		SimulateProcesses S = new SimulateProcesses(10);
+		SimulateProcesses S = new SimulateProcesses(5);
 		initializeProcess(S);
 		S.printProcess();
 		Priority obj = new Priority();
 		obj.scheduleProcesses(S);
 		System.out.println("\nRushang finish");
-
-
-		
+//
+//
+//		
 		System.out.println("\nJuan\n");
 		System.out.println("\nRound Robin Execution\n");
 		RoundRobin R1 = new RoundRobin();
@@ -25,16 +25,28 @@ public class Test {
 
 		
 		
-		//Meet make out put
-		
-		System.out.println("\nMeet");
-		ShortestJobFirst T1 = new ShortestJobFirst();
-//		T1.Test_SPF(S);
+//		**Meet Working Area
+		System.out.println("\nMeet\n");	
+		ShortestJobFirst T1 = new ShortestJobFirst(S);
 		T1.DoShortestJobFirst(S);
-		System.out.println("\nMeet finish\n");
+		
+//		**Printing out using the get methods
+//		float averageWaitTime;
+//		float averageTurnAroundTime;
+//		float averageResponseTime;
+//		int completionTime;
+//		averageWaitTime = T1.getAvgWait();
+//		averageTurnAroundTime = T1.getAvgTurnAround();
+//		averageResponseTime = T1.getAvgResp();
+//		completionTime = T1.getComptTime();
+//		System.out.println("\nAverage TAT: " + averageTurnAroundTime);
+//		System.out.println("Avg Wait Time: " + averageWaitTime);
+//		System.out.println("Average Resp Time" + averageResponseTime);
+//		System.out.println("Comp time: " + completionTime);
+//		System.out.println("\nMeet finish\n");
 
 
-		// Current Wait Time Algorithm
+//		**Current Wait Time Algorithm
 		
 		CurrentWaitTime C = new CurrentWaitTime(S);
 		C.scheduler();
@@ -44,7 +56,7 @@ public class Test {
 
 	
 static void initializeProcess(SimulateProcesses S) {
-		for(int i=1; i<=10; i++) {
+		for(int i=1; i<=5; i++) {
 			Process P = new Process(i);
 			S.addProcesses(P);
 		} //end for
